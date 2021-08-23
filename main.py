09fba2d9f55e3aa88434cd3a26dc03ea4e431d8c
Israel Sagesse <israelsagesse@gmail.com>
2021-08-23 14:09:08 -0400

    adding error checks if it's a name

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from difflib import get_close_matches
 data = json.load(open('data.json'))
 
 def definition():
-    words = input("Enter a word to define: ").lower()
-    if words in data:
-        print(data[words][0])
+    words = input("Enter a word to define: ")
+    if words.lower() in data:
+        print(data[words.lower()][0])
+        if words.title() in data:
+            print(data[words.title()][0])
     elif len(get_close_matches(words, data.keys())) > 0:
-        decision = input(f"Did you mean {get_close_matches(words, data.keys())[0]} instead? Please enter yes or no: ").lower()
+        decision = input(f"Did you mean {get_close_matches(words, data.keys())[0]} instead? Please enter yes or no: ")
         if decision == "yes":
             word = get_close_matches(words, data.keys())[0]
             print(data[word][0])
